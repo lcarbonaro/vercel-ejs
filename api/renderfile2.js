98fc7e6aa906b325a28f4940c61e1209ad89ff52
Les Carbonaro <lcarbonaro@gmail.com>
2020-06-30 21:26:49 -0400

    fixed use of ejs, kinda

--- api/renderfile2.js
+++ api/renderfile2.js
@@ -5,7 +5,7 @@ const { send } = require("micro");
 const { join } = require('path');
 
 const handler = (req, res) => {
-    let html = ejs.renderFile(join('views','test.ejs'), { testMsg: "Hello World From EJS View!!" }, {}, function(err,html) {
+    let html = ejs.renderFile(join('views','test2.ejs'), { testMsg: "Hello World Too From EJS View 2!!" }, {}, function(err,html) {
         if(err) {
             send(res, 200, err);
         } else {
